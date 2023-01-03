0a67a100e8c39c3be8c10a237ca092ab06f0be86
Jonas Hammerschmidt <j.hammerschmidt@krz.de>
2023-01-03 02:36:41 +0100

    Upgraded to Angular 13

--- projects/ngx-kebab/README.md
+++ projects/ngx-kebab/README.md
@@ -1,17 +1,24 @@
-# NGX-Kebab
+# NgxKebab
 
-Kebab loading animation for Angular.
+This library was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.0.
 
-## Use it
+## Code scaffolding
 
-```bash
-npm install ngx-kebab --save 
-```
+Run `ng generate component component-name --project ngx-kebab` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module --project ngx-kebab`.
+> Note: Don't forget to add `--project ngx-kebab` or else it will be added to the default project in your `angular.json` file. 
 
-## Build library
+## Build
 
-This project can be build by using: 
+Run `ng build ngx-kebab` to build the project. The build artifacts will be stored in the `dist/` directory.
 
-```bash
-ng build ngx-kebab
-```
+## Publishing
+
+After building your library with `ng build ngx-kebab`, go to the dist folder `cd dist/ngx-kebab` and run `npm publish`.
+
+## Running unit tests
+
+Run `ng test ngx-kebab` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,31 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
-  }));
-  it('should create the app', async(() => {
+  });
+
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'kebab'`, async(() => {
+  });
+
+  it(`should have as title 'kebab'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app.title).toEqual('kebab');
-  }));
-  it('should render title in a h1 tag', async(() => {
+  });
+
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to kebab!');
-  }));
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('kebab app is running!');
+  });
 });
