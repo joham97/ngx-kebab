2c47d1fdaff7dc1f38787f559bcf3695ec1baa8e
Jonas Hammerschmidt <j.hammerschmidt@krz.de>
2023-01-03 02:44:31 +0100

    Fixed

--- projects/ngx-kebab/src/lib/ngx-kebab.component.ts
+++ projects/ngx-kebab/src/lib/ngx-kebab.component.ts
@@ -3,11 +3,11 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'kebab',
   templateUrl: './ngx-kebab.component.html',
-  styleUrls: ['./ngx-kebab.component.css']
+  styleUrls: ['./ngx-kebab.component.scss']
 })
 export class KebabComponent {
 
-  @Input('size') size: 'small' | 'medium' | 'large';
+  @Input('size') size: 'small' | 'medium' | 'large' = 'medium';
 
   constructor() { }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { KebabModule } from 'projects/ngx-kebab/src/public_api';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    KebabModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
